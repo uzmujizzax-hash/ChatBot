41196e140d24b135e601ab559d7456ebb6d0f469
unknown <uzmujizzax@gmail.com>
2025-10-17 15:14:58 +0500

    config

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ st.markdown("## :rainbow[O'zbekistan Milliy universitetining Jizzax filiali bot 
 
 xabar = st.chat_input("Savolingizni kiriting:")
 
-setup_prompt="""
-Qoidalar:
-Istalgan xabar yozilsa sen o'zingni "Men O'zbekiston Milliy universitetining Jizzax filiali bot yordamchisiman deb ayt. Savollaringizga yordam berishga tayyorman deb aytishing kerak". Keyin foydalanuvchi yozgan ma'lumotlarga javob bersang bo'ladi.
-Matematika, Dasturlash, Fizika, Kimyo, Biologiya, Tarix, Geografiya, Adabiyot va boshqa fanlar bo'yicha savollarga javob bermasliging kerak.
-Sen o'zingni modelingni so'ralgan harqanday savolga javob bermasliging kerak.
-Dasturlash tillari haqida ma'lumot berishdan saqlan.
-Ko'p so'raladigan savollarga javob berishdan saqlan.
-Ko'ngilochar saytlar yoki ilovalar haqida ma'lumot berishdan saqlan.
-Sen https://www.jbnuu.uz yoki http://www.jbnuu.uz saytlaridagi manbaalarga asoslangan holda javob berishing kerak!!!
-Har qanday holda ham dastur tuzish yoki kod yozishdan saqlan.
-"""
-# uploaded_file = st.file_uploader("PDF faylni tanlang", type="pdf")
-# if uploaded_file is not None:
-#     st.pdf(uploaded_file, height=400)
-
 if xabar:
     st.write(xabar)
     placeholder = st.empty()
@@ -32,10 +17,10 @@ if xabar:
     messages = [
     {
         'role': 'user',
-        'content': xabar+setup_prompt,
+        'content': xabar+st.secrets["SETUP_PROMPT"]
     },
     ]
     with st.spinner('Tez orada savolingizga javob beramiz...'):
         for part in client.chat('gpt-oss:120b', messages=messages, stream=True):
             response_text += part['message']['content']
-            placeholder.markdown(response_text)
+            placeholder.markdown(response_text, unsafe_allow_html=True)
